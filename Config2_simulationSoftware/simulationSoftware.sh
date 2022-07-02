7c70ce0e5eee9f160a90986e519dbf4d95f5cd2f
teddy0@ubuntuDocker <tong011@e.ntu.edu.sg>
2022-07-03 02:01:29 +0700

    first commit of simulation software

---
+++ Config2_simulationSoftware/simulationSoftware.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+developmentAppsInstall() {
+	aptInstall git build-essential vim gh
+}
+
+echo "developmentAppsInstall --> installs git, base-devel, vim etc"
+
+vimPluginInstall() {
+	aptInstall vim-youcompleteme \
+		vim-ultisnips \
+		vim-snippets \
+		vim-syntastic \
+		vim-tabular \
+		vim-gitgutter \
+		vim-ctrlp
+}
+
+
+
+echo "vimPluginInstall --> installs various vim plugins"
+
+aptInstall() {
+	apt install $@ -y
+}
+echo "aptInstall [apps] --> installs apps from aptitude with -y flag"
+
+
