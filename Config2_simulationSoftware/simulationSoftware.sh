57d53393852c0cb881f750e55eea636d1b0d5c95
teddy0@ubuntuDocker <tong011@e.ntu.edu.sg>
2022-07-03 02:34:29 +0700

    openfoam install added

--- Config2_simulationSoftware/simulationSoftware.sh
+++ Config2_simulationSoftware/simulationSoftware.sh
@@ -1,24 +1,25 @@
 #!/bin/bash
 
-developmentAppsInstall() {
-	aptInstall git build-essential vim gh
+openfoamInstall () {
+	aptInstall sudo wget curl -y
+	wget -q -O - https://dl.openfoam.com/add-debian-repo.sh | sudo bash
+	sudo apt-get install openfoam2112-default -y
+	sudo dpkg --remove --force-all libopenmpi-dev
+	sudo apt install libopenmpi-dev -y
+	ls /usr/lib/openfoam/openfoam2112/etc/
+	echo "source /usr/lib/openfoam/openfoam2112/etc/bashrc" >> ~/.bashrc
+	echo "source /usr/lib/openfoam/openfoam2112/etc/bashrc" >> ~/.zshrc
 }
 
-echo "developmentAppsInstall --> installs git, base-devel, vim etc"
+echo "openfoamInstall --> installs openfoam and prereqs"
 
-vimPluginInstall() {
-	aptInstall vim-youcompleteme \
-		vim-ultisnips \
-		vim-snippets \
-		vim-syntastic \
-		vim-tabular \
-		vim-gitgutter \
-		vim-ctrlp
+salomeInstall () {
+	aptInstall build-essential 
+	aptInstall pip libboost-all-dev libqwt-qt5-dev libglu1-mesa-dev 
+	aptInstall libqt5printsupport5 libqt5test5 libqt5x11extras5-dev 
+	aptInstall libqt5help5 libfreeimage-dev libxmu-dev libtbb-dev liblapack-dev 
+	aptInstall python3-sip python3-pyqt5 
 }
-
-
-
-echo "vimPluginInstall --> installs various vim plugins"
 
 aptInstall() {
 	apt install $@ -y
